7cd43a03d64b4bd3909325676fdde17bba0e9f8a
Hayate <hayatecht@gmail.com>
2024-06-14 16:16:30 +0200

    print-it

---
+++ oeuvres.php
@@ -0,0 +1,37 @@
+<?php
+    // require_once("oeuvre.php");
+ 
+$articles = array(
+    array(
+        "titre" => "Dodomu",
+        "lien" => "oeuvre-1.html",
+        "image" => "img/clark-van-der-beken.png",
+        "desc" => "Mia Tozerski"
+    ),
+    array(
+        "titre" => "Aashaaheen Baadal",
+        "lien" => "oeuvre-2.html",
+        "image" => "img/pawel-czerwinski-3.png",
+        "desc" => "Anaisha Devi"
+    ),
+    array(
+        "titre" => "Nightlife Traffic",
+        "lien" => "oeuvre-3.html",
+        "image" => "img/dan-cristian-padure.png",
+        "desc" => "Andrew Forsythe"
+    ),
+    array(
+        "titre" => "Le refuge de l'Havre",
+        "lien" => "oeuvre-4.html",
+        "image" => "img/steve-johnson-5.png",
+        "desc" => "Simon Pelletier"
+    ),
+    array(
+        "titre" => "Red Washover",
+        "lien" => "oeuvre-5.html",
+        "image" => "img/steve-johnson.png",
+        "desc" => "Kit Van Der Borght"
+    )
+);
+
+?>
